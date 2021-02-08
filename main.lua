54b4331b60cf17ccf372e1ead3372c0d0869e2ab
Daniel Lopes <daniel.lopes04@gmail.com>
2021-02-08 01:01:31 -0300

    Initial commit

---
+++ main.lua
@@ -0,0 +1,17 @@
+--[[
+    Part of "S50's Intro to Game Development"
+    Lecture 1
+    
+    -- Implementation of mobile game "Flappy Bird" --
+    Made by: Daniel de Sousa
+    https://github.com/daniellopes04
+]]
+
+-- Push library
+-- https://github.com/Ulydev/push
+push = require "push"
+
+-- Class library
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require "class"
+
