bd84398410e20bb9a2e183da12866b4dd7957263
Daniel Lopes <daniel.lopes04@gmail.com>
2021-02-15 16:08:55 -0300

    Better collision with paddle

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -55,8 +55,22 @@ function PlayState:update(dt)
 
     -- Checks collision between ball and paddle
     if self.ball:collides(self.paddle) then
-        -- The ball bounces off paddle
+        -- In case the ball goes below the paddle, raise its y position a little
+        self.ball.y = self.paddle.y - 8
+
+        -- Reverse dy, so it bounces off
         self.ball.dy = -self.ball.dy
+
+        -- We want to tweak the angle of dx a little, based on where the ball hits the paddle
+        -- If we hit the paddle on its left side while moving left...
+        if self.ball.x < self.paddle.x + (self.paddle.width / 2) and self.paddle.x < 0 then
+            self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2) - self.ball.x)
+
+        -- If we hit the paddle on its right side while moving right...
+        elseif self.ball.x > self.paddle.x + (self.paddle.width / 2) and self.paddle.x > 0 then
+            self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2) - self.ball.x)
+        end
+
         gSounds["paddle-hit"]:play()
     end
 
@@ -66,6 +80,40 @@ function PlayState:update(dt)
         if brick.inPlay and self.ball:collides(brick) then
             -- Takes brick out of play
             brick:hit()
+
+            -- Collision for bricks
+            -- We check to see if the opposite side of our velocity is outside of the brick.
+            -- Then we flip it and reset position outside of brick
+
+            -- Left edge; only checks if we're moving right
+            if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x - self.ball.width
+
+            -- Right edge; only check if we're moving left
+            elseif self.ball.x + 6 > brick.x + brick.width and self.ball.dx < 0 then
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x + brick.width
+
+            -- Top edge; always check
+            elseif self.ball.y < brick.y then
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y - self.ball.height
+
+            -- Bottom edge; less likely to occur
+            else
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y + brick.height
+            end
+
+            -- Slightly increases the y velocity to speed up the game
+            -- Maximum velocity is +- 150
+            if math.abs(self.ball.dy) < 150 then
+                self.ball.dy = self.ball.dy * 1.02
+            end
+
+            -- Break so it only allows the ball to collide with one brick at a time
+            break
         end
     end
 
